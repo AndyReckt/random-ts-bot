924574f7b20daa35e89450dac5063513dc55df2b
Andy <alex123aze@gmail.com>
2023-10-11 17:08:35 +0200

    chore: template for info messages

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -7,11 +7,17 @@ const success = () =>
     new EmbedBuilder().setColor(Colors.Green).setTitle("Successful");
 
 /**
+ * Template for info messages
+ */
+const info = () => new EmbedBuilder().setColor(Colors.Blurple).setTitle("Info");
+
+/**
  * Template for error messages
  */
 const error = () => new EmbedBuilder().setColor(Colors.Red).setTitle("Error");
 
 export default {
     success,
+    info,
     error,
 };
